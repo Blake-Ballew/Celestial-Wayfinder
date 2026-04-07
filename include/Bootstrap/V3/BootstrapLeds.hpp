7c535e7a17c748fa0e63c87b0e8a2f921f206442
Blake Ballew <37851606+Blake-Ballew@users.noreply.github.com>
2026-04-07 00:13:00 -0400

    All 3 hardware versions now compile with new LED bootstrapping code

--- include/Bootstrap/V3/BootstrapLeds.hpp
+++ include/Bootstrap/V3/BootstrapLeds.hpp
@@ -19,13 +19,29 @@
 #include "SolidRing.hpp"
 #include "../../HelperClasses/Led/Patterns/Flashlight.hpp"
 
-#define NUM_COMPASS_LEDS 16
-#define NUM_FLASHLIGHT_LEDS 8
-#define NUM_LEDS 30
-#define LED_PIN 27
+
+
+#define NUM_LEDS 61
+#define LED_PIN 16
 #define LED_ORDER GRB
 #define LED_TYPE WS2812B
 #define LED_TASK_CPU_CORE 0
+
+#define LED_IDX_ENCODER_RING 41
+#define NUM_ENCODER_LEDS 8
+
+#define LED_IDX_COMPASS_RING 17
+#define NUM_COMPASS_LEDS 32
+
+#define LED_IDX_LEFT_TRACE 5
+#define LED_IDX_RIGHT_TRACE 57
+#define NUM_TRACE_LEDS 4
+
+#define LED_IDX_POWER_BUTTON 0
+#define LED_IDX_BUTTON_1 4
+#define LED_IDX_BUTTON_2 3
+#define LED_IDX_BUTTON_3 1
+#define LED_IDX_BUTTON_4 2
 
 /*
 LED Mappings:
@@ -39,6 +55,19 @@ LED Mappings:
 17-48:  Compass (Counter-clockwise)
 49-56:  Knob ring (Counter-clockwise)
 57-60:  Right Trace
+
+LED Mappings Post Screen Light Removal:
+TODO: Remap LEDs after bodging
+0:      Power Button
+1:      Button 3
+2:      Button 4
+3:      Button 2
+4:      Button 1
+5-8:  Left Trace
+9-40:  Compass (Counter-clockwise)
+41-48:  Knob ring (Counter-clockwise)
+49-52:  Right Trace
+
 */
 
 class BootstrapLeds
@@ -53,7 +82,6 @@ public:
         LED_Utils::registerPattern(&RingPointPattern());
         LED_Utils::registerPattern(&RingPulsePattern());
         LED_Utils::registerPattern(&ScrollWheelPattern());
-        LED_Utils::registerPattern(&FlashlightPattern());
 
         LED_Manager::init(NUM_LEDS, LEDBuffer(), LED_TASK_CPU_CORE);
 
@@ -81,23 +109,67 @@ public:
 
 #pragma region LED_Segments
 
+    static std::vector<size_t> &CompassRingIndicies()
+    {
+        static std::vector<size_t> compassRingIndicies = 
+        {
+            17, 48, 47, 46, 45, 44, 43, 42, 
+            41, 40, 39, 38, 37, 36, 35, 34, 
+            33, 32, 31, 30, 29, 28, 27, 26, 
+            25, 24, 23, 22, 21, 20, 19, 18
+        };
+
+        return compassRingIndicies;
+    }
+
     static LedSegment &CompassRingSegment()
     {
-        static LedSegment compassRing(LEDBuffer(), 0, NUM_COMPASS_LEDS);
+        static LedSegment compassRing(LEDBuffer(), CompassRingIndicies());
         return compassRing;
+    }
+
+    static std::vector<size_t> EncoderRingIndicies()
+    {
+        static std::vector<size_t> encoderRingIndicies = 
+        {
+            49, 56, 55, 54,
+            53, 52, 51, 50
+        };
+
+        return encoderRingIndicies;
+    }
+
+    static LedSegment &EncoderRingSegment()
+    {
+        static LedSegment encoderRing(LEDBuffer(), EncoderRingIndicies());
+        return encoderRing;
     }
 
     static LedSegment &InputLedSegment()
     {
         // Initialize input LEDs in order of InputID
-        static LedSegment inputLeds(LEDBuffer(), {22, 19, 18, 17, 16, 20, 21});
+        static LedSegment inputLeds(
+            LEDBuffer(),
+            {
+                LED_IDX_BUTTON_1, 
+                LED_IDX_BUTTON_2, 
+                LED_IDX_BUTTON_3, 
+                LED_IDX_BUTTON_4, 
+                LED_IDX_POWER_BUTTON
+            });
         return inputLeds;
     }
 
-    static LedSegment &FlashlightSegment()
+    static LedSegment LeftTraceSegment()
     {
-        static LedSegment flashlight(LEDBuffer(), 23, NUM_FLASHLIGHT_LEDS);
-        return flashlight;
+        static LedSegment leftTrace(LEDBuffer(), LED_IDX_LEFT_TRACE, NUM_TRACE_LEDS);    
+        return leftTrace;
+    }
+
+    static LedSegment RightTraceSegment()
+    {
+        static LedSegment rightTrace(LEDBuffer(), LED_IDX_RIGHT_TRACE, NUM_TRACE_LEDS);    
+        return rightTrace;
     }
 
 #pragma endregion
@@ -113,9 +185,7 @@ public:
                 DisplayModule::InputID::BUTTON_2,
                 DisplayModule::InputID::BUTTON_3,
                 DisplayModule::InputID::BUTTON_4,
-                BUTTON_SOS,
-                DisplayModule::InputID::ENC_UP,
-                DisplayModule::InputID::ENC_DOWN
+                BUTTON_SOS
             });
         return buttonFlash;
     }
@@ -129,9 +199,7 @@ public:
                 DisplayModule::InputID::BUTTON_2,
                 DisplayModule::InputID::BUTTON_3,
                 DisplayModule::InputID::BUTTON_4,
-                BUTTON_SOS,
-                DisplayModule::InputID::ENC_UP,
-                DisplayModule::InputID::ENC_DOWN
+                BUTTON_SOS
             });
         return illuminateButton;
     }
@@ -142,22 +210,28 @@ public:
         return ringPoint;
     }
 
+    static RingPoint &EncoderPointPattern()
+    {
+        static RingPoint encoderPoint(CompassRingSegment());
+        return encoderPoint;
+    }
+
     static RingPulse &RingPulsePattern()
     {
         static RingPulse ringPulse(CompassRingSegment());
         return ringPulse;
     }
 
-    static ScrollWheel &ScrollWheelPattern()
+    static RingPulse &EncoderPulsePattern()
     {
-        static ScrollWheel scrollWheel(CompassRingSegment());
-        return scrollWheel;
+        static RingPulse encoderPulse(EncoderRingSegment());
+        return encoderPulse;
     }
 
-    static Flashlight &FlashlightPattern()
+    static ScrollWheel &ScrollWheelPattern()
     {
-        static Flashlight flashlight(FlashlightSegment());
-        return flashlight;
+        static ScrollWheel scrollWheel(EncoderRingSegment());
+        return scrollWheel;
     }
 
 #pragma endregion
